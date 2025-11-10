5e765beecebf09929ba070240e4d356847b7c69e
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-11-10 22:12:59 +0530

    updated commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Time variables
+let timeLeft = 25 *  60; // 25 min in seconds
+let timerId = null;
+let isRunning = false;
+let isWorkTime = true;
+
+// Get DOM elements
+const timer = document.getElementById('timer');
+const pauseBtn = document.getElementById('pause');
+const playBtn = document.getElementById('play');
+const resetBtn = document.getElementById('reset');
+const workBtn = document.getElementById('work');
+const breakBtn = document.getElementById('break');
+
+// ***TODO ----
+// Update timer display
+
+// start timer
+
+// Pause timer
+
+// Reset timer
+
+// Switch to work mode
+
+// Switch to break mode
+
+// Event Listeners
+
+// Initial display
